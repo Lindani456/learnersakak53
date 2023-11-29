4bbf1ae966c41914d510423ce20895380d154139
vusumuzi <vgwala149@gmail.com>
2023-11-29 13:37:22 +0200

    make a pdf of the result

--- src/app/result/result.page.ts
+++ src/app/result/result.page.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
 import { Subscription } from 'rxjs';
+///import * as pdfMake from 'pdfmake/build/pdfmake';
+// @ts-ignore
+const pdfMake = require('pdfmake/build/pdfmake.js');
+// @ts-ignore
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
 
 @Component({
   selector: 'app-result',
@@ -42,7 +47,89 @@ export class ResultPage implements OnInit {
       this.routeSubscription.unsubscribe();
     }
   }
+  async generatePDF() {
+    console.log('Score:', this.score);
+    // Define the content for the PDF
+    pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
+     let pdfContent = {
+      content: [
+        {
+          text: 'Test Result',
+          style: 'header',
+        },
+        {
+          text: `Time: ${this.time}`,
+          margin: [0, 0, 0, 10], // Add margin to separate from the header
+        },
+        {
+          text: `Your Score: ${this.score.toFixed(2)}%`,
+          style: 'score',
+          margin: [0, 0, 0, 20], // Add margin below the score
+        },
+        {
+          text: 'Correct Answers',
+          style: 'answer-status correct',
+          margin: [0, 0, 0, 10], // Add margin above correct answers
+        },
+        {
+          ul: this.correctAnswers.map(answer => answer.text),
+          margin: [0, 0, 0, 10], // Add margin below correct answers
+        },
+        {
+          text: 'Incorrect Answers',
+          style: 'answer-status incorrect',
+          margin: [0, 0, 0, 10], // Add margin above incorrect answers
+        },
+        {
+          ul: this.incorrectAnswers.map(answer => `${answer.text} (Your answer: ${answer.selectedAnswer})`),
+          margin: [0, 0, 0, 10], // Add margin below incorrect answers
+        },
+        {
+          text: 'Unanswered Questions',
+          style: 'answer-status unanswered',
+          margin: [0, 0, 0, 10], // Add margin above unanswered questions
+        },
+        {
+          ul: this.unansweredQuestions.map(question => question.text),
+          margin: [0, 0, 0, 10], // Add margin below unanswered questions
+        },
+      ],
+      styles: {
+        header: {
+          fontSize: 18,
+          bold: true,
+          margin: [0, 0, 0, 10],
+        },
+        score: {
+          fontSize: 16, // Use an appropriate numeric value
+          bold: true,
+          margin: [0, 0, 0, 20],
+        },
+        
+        
+        'answer-status': {
+          bold: true,
+          margin: [0, 0, 0, 10],
+        },
+        correct: {
+          color: '#4caf50', // green
+        },
+        incorrect: {
+          color: '#f44336', // red
+        },
+        unanswered: {
+          color: '#ff9800', // orange
+        },
+      },
+    };
+  
+      // Generate the PDF
+      const pdfDocGenerator = await pdfMake.createPdf(pdfContent);
 
+      const blob = await new Promise<Blob>((resolve) => {
+        pdfDocGenerator.download();
+      });
+    }
  
 }
